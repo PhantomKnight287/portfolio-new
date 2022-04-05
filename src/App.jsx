5e6509bda977d03035cd73e3cb5cb9af176d43fa
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-06 00:16:13 +0530

    add partytown script in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
 import { Containered } from "./Components";
-
+import { Partytown } from "@builder.io/partytown/react";
 function App() {
   return (
     <>
       <Containered />
+      <Partytown debug={true} forward={["dataLayer.push"]} />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+    
+          gtag("config", "G-285TD5F9QW");
+          `,
+        }}
+      ></script>
     </>
   );
 }
