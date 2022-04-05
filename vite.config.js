2e92d0d81a3e05b0d1737d98aaf467438d7b4905
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-06 00:13:53 +0530

    add partytown will configure later

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { partytownVite } from "@builder.io/partytown/utils";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    plugins: [
+      partytownVite({
+        dest: path.join(__dirname, "dist", "~partytown"),
+      }),
+    ],
+  },
+});
